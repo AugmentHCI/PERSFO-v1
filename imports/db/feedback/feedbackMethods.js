0f8bd11fca459bb82e8073a9f25516c704cf18f8
Robin De Croon <robindecroon@msn.com>
2021-03-31 10:23:50 +0200

    code refactoring

---
+++ imports/db/feedback/feedbackMethods.js
@@ -0,0 +1,23 @@
+import { check } from 'meteor/check';
+import { FeedbackCollection } from '/imports/db/FeedbackCollection';
+
+Meteor.methods({
+    "feedback.submitNewFeedback"(feedback) {
+        check(feedback, String);
+    
+        if (!this.userId) {
+          throw new Meteor.Error("Not authorized.");
+        }
+    
+        const userPreferences = UserPreferences.findOne({ userid: this.userId });
+        const recommendations = RecommendedRecipes.findOne({ userid: this.userId });
+    
+        FeedbackCollection.insert({
+          userid: this.userId,
+          feedback: feedback,
+          timestamp: new Date(),
+          userPreferences: userPreferences,
+          recommendedRecipes: recommendations,
+        });
+      }
+})
