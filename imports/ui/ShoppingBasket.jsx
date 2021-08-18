5270e4367f193755ffda60884232a6e030189a44
Robin De Croon <robindecroon@msn.com>
2021-08-18 17:14:22 +0200

    update shopping basket

--- imports/ui/ShoppingBasket.jsx
+++ imports/ui/ShoppingBasket.jsx
@@ -1,5 +1,6 @@
 import Avatar from '@material-ui/core/Avatar';
-import Checkbox from '@material-ui/core/Checkbox';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { Button } from '@material-ui/core';
 import Container from "@material-ui/core/Container";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -7,13 +8,13 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from "@material-ui/core/styles";
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { Meteor } from "meteor/meteor";
 import { useTracker } from "meteor/react-meteor-data";
 import React from "react";
 import { getImage } from '../api/apiPersfo';
 import { OrdersCollection } from '/imports/db/orders/OrdersCollection';
 import { RecipesCollection } from '/imports/db/recipes/RecipesCollection';
-import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 
 const useStyles = makeStyles((persfoTheme) => ({
@@ -27,6 +28,31 @@ const useStyles = makeStyles((persfoTheme) => ({
     },
 }));
 
+class GroupedButtons extends React.Component {
+    state = { counter: 1 };
+
+    handleIncrement = () => {
+        this.setState(state => ({ counter: state.counter + 1 }));
+    };
+
+    handleDecrement = () => {
+        if (this.state.counter > 0) {
+            this.setState(state => ({ counter: state.counter - 1 }));
+        }
+    };
+    render() {
+        const displayCounter = this.state.counter > 0;
+
+        return (
+            <ButtonGroup size="small" aria-label="small outlined button group">
+                <Button onClick={this.handleIncrement}>+</Button>
+                <Button variant="contained" disabled>{this.state.counter}</Button>
+                <Button onClick={this.handleDecrement}>-</Button>
+            </ButtonGroup>
+        );
+    }
+}
+
 const componentName = "ShoppingBasket";
 export const ShoppingBasket = () => {
     const classes = useStyles();
@@ -35,19 +61,19 @@ export const ShoppingBasket = () => {
         const noDataAvailable = { orders: [] };
         const handler = Meteor.subscribe("orders");
         if (!handler.ready()) {
-          return { ...noDataAvailable };
+            return { ...noDataAvailable };
         }
-    
+
         const orders = OrdersCollection.find({
-          userid: Meteor.userId()
+            userid: Meteor.userId()
         }).fetch();
         console.log(orders);
         return { orders };
-      });
+    });
 
     const handleRemove = (order) => () => {
         console.log(order);
-      Meteor.call('orders.handleOrder', order.recipeId);
+        Meteor.call('orders.handleOrder', order.recipeId);
     };
 
     return (
@@ -64,11 +90,12 @@ export const ShoppingBasket = () => {
                                 />
                             </ListItemAvatar>
                             <ListItemText id={labelId} primary={RecipesCollection.findOne({ id: value.recipeId }).name} />
-                            <ListItemSecondaryAction>
+                            <GroupedButtons></GroupedButtons>
+                            {/* <ListItemSecondaryAction>
                                 <HighlightOffIcon
                                     onClick={handleRemove(value)}
                                 />
-                            </ListItemSecondaryAction>
+                            </ListItemSecondaryAction> */}
                         </ListItem>
                     );
                 })}
